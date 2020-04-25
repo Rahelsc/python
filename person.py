d06756fffba9fde12a536f055b0d30ed062b8007
Rahel Schwartz <Rahelsc@gmail.com>
2020-04-25 19:16:11 +0000

    updated to include question 3. added some error catching

--- person.py
+++ person.py
@@ -1,11 +1,18 @@
+from builtins import property
+from datetime import datetime, date
+
+
 class Person:
-    def __init__(self, fname, lname, birthdate, adress, telephone, email):
+    def __init__(self, fname, lname, birthdate: str, adress, telephone, email):
         self.fname = fname
         self.lname = lname
         self.birthdate = birthdate
         self.adress = adress
         self.telephone = telephone
         self.email = email
+        today = date.today()
+        self.age = today.year - self.birthdate.year - ((today.month, today.day) < (self.birthdate.month,
+                                                                                   self.birthdate.day))
 
     def __str__(self):
         return f'שמי הפרטי הוא:{self.fname}' \
@@ -22,9 +29,29 @@ class Person:
     def fname(self, fname):
         self.__fname = fname
 
+    @property
+    def birthdate(self):
+        return self.__birthdate
+
+    # noinspection PyAttributeOutsideInit
+    @birthdate.setter
+    def birthdate(self, birthdate):
+        try:
+            year, month, day = map(int, birthdate.split(','))
+            self.__birthdate = datetime(year, month, day)
+            return self.__birthdate
+        except ValueError:
+            print("פורמט לא תואם, נסה שוב")
+
+    def calculate_age(self):
+        today = date.today()
+        self.age = today.year - self.birthdate.year - ((today.month, today.day) < (self.birthdate.month,
+                                                                                   self.birthdate.day))
+        return self.age
+
 
 class Student(Person):
-    def __init__(self, fname, lname, birthdate, adress, telephone, email, courses: list, year):
+    def __init__(self, fname, lname, birthdate: str, adress, telephone, email, courses: list, year):
         super().__init__(fname, lname, birthdate, adress, telephone, email)
         self.courses = courses
         self.year = year
@@ -39,14 +66,14 @@ class Student(Person):
 
 
 class Employee(Person):
-    def __init__(self, fname, lname, birthdate, adress, telephone, email, base_salary, seniority):
+    def __init__(self, fname, lname, birthdate: str, adress, telephone, email, base_salary, seniority):
         super().__init__(fname, lname, birthdate, adress, telephone, email)
         self.base_salary = base_salary
         self.seniority = seniority
 
 
 class Lecturer(Employee):
-    def __init__(self, fname, lname, birthdate, adress, telephone, email, base_salary, seniority
+    def __init__(self, fname, lname, birthdate: str, adress, telephone, email, base_salary, seniority
                  , hourly_rate, teaches_courses: list):
         super().__init__(fname, lname, birthdate, adress, telephone, email, base_salary, seniority)
         self.hourly_rate = hourly_rate
@@ -61,22 +88,30 @@ class Lecturer(Employee):
         self.__teaches_courses = teaches_courses
 
 
-student1 = Student('rahel', 'schwartz', '13/6/2020', 'petch tikva', '054-6472701'
+student1 = Student('rahel', 'schwartz', '1988,6,13', 'petch tikva', '054-6472701'
                    , 'rahelsc@gmail.com', ['python', 'js'], 2)
 
-student2 = Student('ronen', 'schwartz', '13/6/2020', 'petch tikva', '054-6472701'
+student2 = Student('ronen', 'schwartz', '1988,6,13', 'petch tikva', '054-6472701'
                    , 'rahelsc@gmail.com', ['python', 'cSharp'], 2)
 
-lecturer = Lecturer("prof", 'fessor', '5/9/1970', 'ramat hasharon', '08-6880255', 'scwartz123@gmail.com', '8000', 'high'
-                    , '200', ['python'])
+lecturer1 = Lecturer("prof", 'fessor', '1970,7,5', 'ramat hasharon', '08-6880255', 'scwartz123@gmail.com', '8000',
+                     'high'
+                     , '200', ['python'])
 
 
 def check_who_teaches(teacher: Lecturer, s: Student):
-    for i in teacher.teaches_courses:
-        for j in s.courses:
-            if i == j:
-                print(f'{teacher.fname} teaches {i}')
-                print(f'{s.fname} studies {i}')
+    try:
+        for i in teacher.teaches_courses:
+            for j in s.courses:
+                if i == j:
+                    print(f'{teacher.fname} teaches {i}')
+                    print(f'{s.fname} studies {i}')
+    except TypeError:
+        print("only recieves a lecturer and a student, please try again")
+    except:
+        print("bad input, try again")
 
 
-check_who_teaches(lecturer, student1)
+check_who_teaches(lecturer1, student1)
+
+print(student1.age)
